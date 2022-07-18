b8f2fd86ae5e06b826004f5d97d5fa1d62a1aeae
supawasa <supawasa@scg.com>
2022-07-18 17:03:42 +0700

    adjust indentation to Tab and add sdk folder for example

--- component_driver/ppg/afe4404/afe4404_reg_map.h
+++ component_driver/ppg/afe4404/afe4404_reg_map.h
@@ -7,78 +7,82 @@
 /* AFE4404 Registers*/
 
 //Diagnosis
-#define DIAGNOSIS       0x00
+#define DIAGNOSIS         0x00
+#define DIAGNOSIS_SWRESET 1<<3
+#define DIAGNOSIS_CNRESET 1<<1
+#define DIAGNOSIS_WRMODE  0<<0
+#define DIAGNOSIS_RDMODE  1<<0
 
 //PRPCT ( timer counter )
-#define PRPCT            0x1D   /**< Bits 0-15 for writing counter value      */
+#define PRPCT             0x1D   /**< Bits 0-15 for writing counter value      */
 
 //Timer Module enable / NUMAV ( # of times to sample and average )
-#define TIM_NUMAV        0x1E
+#define TIM_NUMAV         0x1E
 
-#define TIA_GAINS1       0x21
+#define TIA_GAINS1        0x21
 
 //TIA Gains 2
-#define TIA_GAINS2       0x20
+#define TIA_GAINS2        0x20
 
 
 //Sample LED2 Start / End
-#define LED2STC         0x01
-#define LED2ENDC        0x02
+#define LED2STC           0x01
+#define LED2LEDENDC       0x02
 
 //LED1 Start / End
-#define LED1LEDSTC      0x03
-#define LED1LEDENDC     0x04
+#define LED1LEDSTC        0x03
+#define LED1LEDENDC       0x04
 
 //Sample Ambient 2 ( or LED3 ) Start / End
-#define LED3STC     0x05
-#define LED3ENDC    0x06
+#define LED3STC           0x05
+#define LED3ENDC          0x06
 
 //Sample LED1 Start / End
-#define LED1STC     0x07
-#define LED1ENDC    0x08
+#define LED1STC           0x07
+#define LED1ENDC          0x08
 
 //LED2 Start / End
-#define LED2LEDSTC          0x09
-#define LED2LEDENDC         0x0A
+#define LED2LEDSTC        0x09
+#define LED2LEDENDC       0x0A
 
 //Sample Ambient 1 Start / End
-#define ALED1STC     0x0B
-#define ALED1ENDC    0x0C
+#define ALED1STC          0x0B
+#define ALED1ENDC         0x0C
 
 //LED2 Convert Start / End
-#define LED2CONVST     0x0D
-#define LED2CONVEND    0x0E
+#define LED2CONVST        0x0D
+#define LED2CONVEND       0x0E
 
 //Ambient 2 ( or LED3 ) Convert Start / End
-#define LED3CONVST     0x0F
-#define LED3CONVEND    0x10
+#define LED3CONVST        0x0F
+#define LED3CONVEND       0x10
 
 //LED1 Convert Start / End
-#define LED1CONVST     0x11
-#define LED1CONVEND    0x12
+#define LED1CONVST        0x11
+#define LED1CONVEND       0x12
 
 //Ambient 1 Convert Start / End
-#define ALED1CONVST     0x13
-#define ALED1CONVEND    0x14
+#define ALED1CONVST       0x13
+#define ALED1CONVEND      0x14
 
 //ADC Reset Phase 0 Start / End
-#define ADCRSTSTCT0    0x15
-#define ADCRSTENDCT0   0x16
+#define ADCRSTSTCT0       0x15
+#define ADCRSTENDCT0      0x16
 
 //ADC Reset Phase 1 Start / End
-#define ADCRSTSTCT1    0x17
-#define ADCRSTENDCT1   0x18
+#define ADCRSTSTCT1       0x17
+#define ADCRSTENDCT1      0x18
 
 //ADC Reset Phase 2 Start / End
-#define ADCRSTSTCT2    0x19
-#define ADCRSTENDCT2   0x1A
+#define ADCRSTSTCT2       0x19
+#define ADCRSTENDCT2      0x1A
 
 //ADC Reset Phase 3 Start / End
-#define ADCRSTSTCT3    0x1B
-#define ADCRSTENDCT3   0x1C
+#define ADCRSTSTCT3       0x1B
+#define ADCRSTENDCT3      0x1C
 
 //LED Current Control
-#define LED_CONFIG       0x22
+#define LED_CONFIG        0x22
 /* ********************************************************************
  * LED current control is a 24 bit register where                     *
  * LED1: bits 0-5 LED2: bits 6-11 LED3: bits 12-17 and the rest are 0 *
@@ -93,11 +97,11 @@
  **********************************************************************/
 
 
-#define SETTINGS          0x23  /**< Settings Address */
-#define SETTINGS_AFE_EN   1<<0 //Entire AFE is powered down
-#define SETTINGS_AFE_DIS  0<<0 //Normal mode
-#define SETTINGS_RX_EN    1<<1 //RX portion of the AFE is powered do
-#define SETTINGS_RX_DIS   0<<1 //Normal mode
+#define SETTINGS           0x23  /**< Settings Address */
+#define SETTINGS_AFE_EN    1<<0 //Entire AFE is powered down
+#define SETTINGS_AFE_DIS   0<<0 //Normal mode
+#define SETTINGS_RX_EN     1<<1 //RX portion of the AFE is powered do
+#define SETTINGS_RX_DIS    0<<1 //Normal mode
 #define SETTINGS_DY4_EN    1<<3 //Rest of ADC is powered down in dynamic power-down mode
 #define SETTINGS_DY4_DIS   0<<3 //Rest of ADC is not powered down
 #define SETTINGS_DY3_EN    1<<4 //TIA is powered down in dynamic power-down mode
@@ -106,7 +110,7 @@
 #define SETTINGS_OSC_DIS   0<<9 // External clock mode
 #define SETTINGS_DY2_EN    1<<14 //ADC is powered down in dynamic power-down mode
 #define SETTINGS_DY2_DIS   0<<14 //ADC is not powered down
-#define SETTINGS_ILED_100    1<<17 //LED current range is 0 mA to 100 mA
+#define SETTINGS_ILED_100  1<<17 //LED current range is 0 mA to 100 mA
 #define SETTINGS_ILED_50   0<<17 //LED current range is 0 mA to 50 mA
 #define SETTINGS_DY1_EN    1<<20 //Transmitter is powered down in dynamic power-down mode
 #define SETTINGS_DY1_DIS   0<<20 //Transmitter is not powered down
